3ad893ec08d9df7b89d13bf469aa51f570858795
Alan Maqueda <maqalher@hotmail.com>
2024-08-16 15:39:05 -0600

    first commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+ title: 'Contact Page',
+ description: 'Contact Title',
+};
+
+export default function ContactPage(){
+    return(
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
